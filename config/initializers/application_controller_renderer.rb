61abeead48ce54dcdfcd41367b96c0bb2421f871
岩本空竜 <akeruiwamoto@akeru.local>
2020-06-06 13:27:32 +0900

    first commit

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,11 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+
+Refile.secret_key = '68efa0fdcd80396fdf2a7d4325680468345cb7014e09c7c14ead8617832d1aa31172a0350ade6098997645487f36fa1c735764c9ba4a0e21ab0d66b545415e10'
